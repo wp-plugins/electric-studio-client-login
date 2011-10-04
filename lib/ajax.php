dd7f35c2edebdf358fcb1bdfb3904dcc31fb455c
irvingswiftj <irvingswiftj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-04 15:10:16 +0000

    tagging 0.7.1
    git-svn-id: https://plugins.svn.wordpress.org/electric-studio-client-login/tags/0.7.1@447138 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/ajax.php
@@ -0,0 +1,140 @@
+<?php
+
+if( is_admin() ){
+	//add ajax hooks if logged in as admin
+	add_action('wp_print_footer_scripts','escl_print_livesearchjs');
+	add_action('wp_ajax_nopriv_esclusersearch','escl_userlivesearch');
+	add_action('wp_ajax_esclusersearch','escl_userlivesearch');
+	add_action('wp_ajax_nopriv_escladdusertogroup','escl_addusertogroup');
+	add_action('wp_ajax_escladdusertogroup','escl_addusertogroup');
+	add_action('wp_ajax_nopriv_esclremoveuserfromgroup','escl_removeuserfromgroup');
+	add_action('wp_ajax_esclremoveuserfromgroup','escl_removeuserfromgroup');
+}
+
+function escl_print_livesearchjs(){
+	//nonces for ajax
+	$esclLiveSearch_nonce = wp_create_nonce('esclLS');
+	$esclu2gadd_nonce = wp_create_nonce('esclu2gadd');
+	$esclufgrm_nonce = wp_create_nonce('esclufgrm');
+	?>
+	<script type="text/javascript">
+		function escl_usersearch(criteria) {
+			jQuery.ajax({
+				type: "post",
+				url: "<?php echo get_admin_url(); ?>admin-ajax.php",
+				data: {
+					action: 'esclusersearch',
+					criteria: criteria,
+					_ajax_nonce: '<?php echo $esclLiveSearch_nonce; ?>'
+				},
+				beforeSend: function () {
+					jQuery('div#user-search-results-loading').show();
+				},
+				complete: function () {
+					jQuery('div#user-search-results-loading').hide();
+				},
+				success: function (html) { //so, if data is retrieved, store it in html
+					jQuery('div#user-search-results').show();
+					jQuery('div#user-search-results').html(html);
+				},
+				error: function () {
+					alert('There has been an error, Please try again');
+					return false;
+				}
+			});
+		}
+		
+		function escl_addusertogroup(userid) {
+			jQuery.ajax({
+				type: "post",
+				url: "<?php echo get_admin_url(); ?>admin-ajax.php",
+				data: {
+					action: 'escladdusertogroup',
+					userid: userid,
+					groupslug: '<?php echo $_GET['edit_group']; ?>',
+					_ajax_nonce: '<?php echo $esclu2gadd_nonce; ?>'
+				},
+				beforeSend: function () {
+					jQuery('div#user-search-results-loading').show();
+				},
+				complete: function () {
+					jQuery('div#user-search-results-loading').hide();
+				},
+				success: function (html) { //so, if data is retrieved, store it in html
+					jQuery('<li>').html(html).appendTo('ul#group-user-list').hide().fadeIn();
+				},
+				error: function () {
+					alert('There has been an error, Please try again');
+					return false;
+				}
+			});
+		}
+		
+		function escl_removeuserfromgroup(userid) {
+			jQuery.ajax({
+				type: "post",
+				url: "<?php echo get_admin_url(); ?>admin-ajax.php",
+				data: {
+					action: 'esclremoveuserfromgroup',
+					userid: userid,
+					groupslug: '<?php echo $_GET['edit_group']; ?>',
+					_ajax_nonce: '<?php echo $esclufgrm_nonce; ?>'
+				},
+				beforeSend: function () {
+					//jQuery('div#user-search-results-loading').show();
+				},
+				complete: function () {
+					//jQuery('div#user-search-results-loading').hide();
+				},
+				success: function (html) { //so, if data is retrieved, store it in html
+					jQuery('ul#group-user-list li.userid-' + userid).fadeOut(function () {
+						jQuery(this).remove();
+					});
+				},
+				error: function () {
+					alert('There has been an error, Please try again');
+					return false;
+				}
+			});
+		}
+	</script>
+<?php } 
+
+function escl_userlivesearch(){
+	wp_nonce_field('esclLS');
+	$clients = get_users(array(
+		'role' => 'client',
+		'search' => '*'.$_POST['criteria'].'*'
+		)
+	);
+
+	$html .= '<ul>';
+	foreach($clients as $client){
+		$html.=	'<li class="'.$client->user_nicename.'-'.$client->ID.'"><a href="#" class="liveUserResult userID-'.$client->ID.'">'.$client->display_name.'</a></li>'."\n";
+	}
+	$html .= '</ul>';
+			
+	echo $html;
+	die;
+}
+
+function escl_addusertogroup(){
+	wp_nonce_field('esclu2gadd');
+	$groupinfo = escl_get_group_data($_POST['groupslug']);
+	$userid = $_POST['userid'];
+	$userinfo = get_userdata($userid);
+	
+	escl_add_user_to_group($groupinfo->group_id,$userinfo->user_nicename);
+	
+	echo $userinfo->user_login;
+	die;
+}
+
+function escl_removeuserfromgroup(){
+	wp_nonce_field('esclufgrm');
+	$groupinfo = escl_get_group_data($_POST['groupslug']);
+	$userid = $_POST['userid'];
+	$userinfo = get_userdata($userid);
+	escl_remove_user_from_group($groupinfo->group_id,$userinfo->user_login);
+	die;	
+}
---
+++ lib/functions.php
@@ -0,0 +1,66 @@
+<?php
+
+add_shortcode('escl_login_panel','escl_custom_login');
+
+//this function is outputs a login form.
+function escl_custom_login(){ ?>
+			<?php global $user_ID, $user_identity, $user_level ?>
+			<?php if ( $user_ID ) : ?>
+			Logged in as <strong><?php echo $user_identity ?></strong>.
+			<ul id="escl-control-panel">
+				<li><a href="<?php bloginfo('url') ?>/wp-admin/"><?php echo __('Dashboard')?></a></li>
+				<?php if ( $user_level >= 1 ) : ?>
+				<li><a href="<?php bloginfo('url') ?>/wp-admin/post-new.php"><?php echo __('Add a Post')?></a></li>
+				<?php endif // $user_level >= 1 ?>
+				<li><a href="<?php echo escl_get_client_area_perm(); ?>"><?php echo __('My Area')?></a></li>
+				<li><a href="<?php bloginfo('url') ?>/wp-admin/profile.php"><?php echo __('Edit Profile')?></a></li>
+				<li><a href="<?php bloginfo('url') ?>/wp-login.php?action=logout&amp;redirect_to=<?php echo urlencode($_SERVER['REQUEST_URI']) ?>"><?php echo __('Log Out')?></a></li>
+			</ul>
+	
+			<?php else : ?>
+	
+			<form action="<?php bloginfo('url') ?>/wp-login.php" method="post">
+				<p>
+				<label for="log"><input type="text" name="log" id="log" value="<?php echo wp_specialchars(stripslashes($user_login), 1) ?>" size="22" /> User</label><br />
+				<label for="pwd"><input type="password" name="pwd" id="pwd" size="22" /> Password</label><br />
+				<input type="submit" name="submit" value="Send" class="button" />
+				<label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> Remember me</label><br />
+				</p>
+				<input type="hidden" name="redirect_to" value="<?php echo $_SERVER['REQUEST_URI']; ?>"/>
+			</form>
+			<ul>
+				<?php if ( get_option('users_can_register') ){ ?>
+					<li><a href="<?php bloginfo('url') ?>/wp-register.php">Register</a></li>
+				<?php } ?>
+				<li><a href="<?php bloginfo('url') ?>/wp-login.php?action=lostpassword">Recover password</a></li>
+			<?php endif ?>
+			</ul>
+<?php }
+
+//this is the callback function  for the login widget
+function escl_widget_login($args) {
+	extract($args);
+	echo $before_widget;
+	echo $before_title;?>Login<?php echo $after_title;
+	escl_custom_login();
+	echo $after_widget;
+}
+
+
+//this function returns the permlinks of the page with the title of 'Client Area'
+function escl_get_client_area_perm(){
+	global $wpdb;
+	$pageTitle = 'Client Area';
+	
+	$sql = "SELECT ID FROM $wpdb->posts
+		WHERE ( post_name = '".$pageTitle."' OR post_title = '".$pageTitle."' ) 
+		AND post_status = 'publish'
+		AND post_type='page'";
+	
+	$sql = $wpdb->prepare($sql);
+	
+	$pageID = $wpdb->get_var($sql);
+	
+	return get_permalink($pageID);
+}
+
